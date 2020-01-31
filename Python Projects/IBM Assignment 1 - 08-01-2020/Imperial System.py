beba11cce85105daff184b039215950b30031a96
Sparsh Agarwal <sparshaga25@gmail.com>
2020-01-31 15:54:44 +0530

    Solving a BUG

--- Python Projects/IBM Assignment 1 - 08-01-2020/Imperial System.py
+++
@@ -1,9 +0,0 @@
-# This Program converts Length in Imperial syste to Metric System.
-
-metre = float(input())
-inch = 39.45 * metre
-foot = inch//12
-inch = int(round(inch%12))
-yard = int(foot//3)
-foot = int(foot%3)
-print(yard,foot,inch,sep="\n")
